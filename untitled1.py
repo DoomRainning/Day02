86cacb8e35a2c002ff463348f5841be582f52197
DoomRainning <39868183+DoomRainning@users.noreply.github.com>
2018-06-03 11:12:57 +0800

    day02
    未来5天郑州的天气简略

--- untitled1.py
+++ untitled1.py
@@ -22,6 +22,8 @@ for j in range(0,5):
     for i in range(0,7):
         print('时间：'+data['list'][i+j*8]['dt_txt']+'\t')
         print('最高气温~最低温度：'+str(data['list'][i+j*8]['main']['temp_max'])+'开式度'+'~'+str(data['list'][i+j*8]['main']['temp_max'])+'开式度'+'\t')
+        print('实时温度：'+str(data['list'][i+j*8]['main']['temp'])+'开式度'+'\t')
+        print('气压：'+str(data['list'][i+j*8]['main']['pressure'])+'帕'+'\t')
         print('天气状况：'+data['list'][i+j*8]['weather'][0]['description']+'\t')
         print('湿度：'+str(data['list'][i+j*8]['main']['humidity'])+'\t')
         print('\t'*2)
